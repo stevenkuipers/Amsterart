4311fdc21398d0236a273ee3045c37781a9655ec
Steven Kuipers <steven.kuipers@gmail.com>
2023-04-22 14:41:18 +0200

    added alt text and optional reply if longer description is available

--- bot.js
+++ bot.js
@@ -16,13 +16,13 @@ const M = new Mastodon({
     client_secret : process.env.CLIENT_SECRET,
     client_key : process.env.CLIENT_KEY,
     api_url: 'https://botsin.space/api/v1/'
-})
+});
 
 const rijks_url = 'https://www.rijksmuseum.nl/api/en/collection/'
 const params = new URLSearchParams( {'imgonly': 'true', 'format': 'json', 'key': process.env.RIJKS_KEY});
  
 // First request - get all data
-const response = await fetch(`${rijks_url}?${params.toString()}`);
+let response = await fetch(`${rijks_url}?${params.toString()}`);
 const { facets } = await response.json();
 
 // map all keys of the facets key
@@ -34,47 +34,61 @@ const { facets } = await response.json();
 //     ]
 // }, query: 'material',
 // values: [ ...
+
 const keys = facets.map( ({name, facets}) => ( {'query': name, 'values': facets} ) );
 const {query, values} = keys[rndIndex(keys)]; // Randomly select a query and key from the first response to make a request for a random piece of art
-const {key, value} = values[rndIndex(values)]
+const {key, value} = values[rndIndex(values)];
 
-params.append(query, key)
-params.append('ps', value)
+params.append(query, key);
+params.append('ps', value);
 
-const response2 = await fetch(`${rijks_url}?${params.toString()}`);
-const { artObjects } = await response2.json();
+response = await fetch(`${rijks_url}?${params.toString()}`);
+const { artObjects } = await response.json();
 const {links, webImage} = artObjects[rndIndex(artObjects)]; // Here there be art
 
 // Get additional description for the piece
-const descResponse = await fetch(`${links.self}?${params}`);
-const { artObject } = await descResponse.json();
+response = await fetch(`${links.self}?${params}`);
+const { artObject } = await response.json();
 const { title, scLabelLine, plaqueDescriptionEnglish } = artObject;
 
-const res = await fetch(webImage.url);
-const resBuffer = await res.buffer();
+response = await fetch(webImage.url);
+const resBuffer = await response.buffer();
 await ShrinkSize(resBuffer, webImage.width, webImage.height);
 
-// Todo: generate text for status in a seperate function and check char limit (500chars). 
-
 // Post to Mastodon
-M.post('media', { file: fs.createReadStream('images/output.jpg') }).then(resp => {
+M.post('media', { 
+    'file': fs.createReadStream('images/output.jpg'),
+    'description': `${title}`,
+    'remote_url': `${webImage.url}`
+  }).then(resp => {
     const id = resp.data.id;
     M.post('statuses', { 
-        status: `${title}, ${scLabelLine}.\nSource: ${links.web}`, 
-        media_ids: [id] 
+        'status': `${title} \n ${scLabelLine}.\n\nSource: ${links.web}`, 
+        'media_ids': [id],
     },
     function(err, data){
         if(err){
             console.error(err)
         }else {
+            if( plaqueDescriptionEnglish){
+              M.post('statuses', {
+                'status':  `${plaqueDescriptionEnglish.slice(0, 500)}`,
+                'in_reply_to_id': data.id
+              },
+              function(err, data){
+                if(err){
+                  console.error(err)
+                }else{
+                  console.log(`Success, ${plaqueDescriptionEnglish} was posted in reply to ${data.id}`)
+                }
+              })
+            }
             console.log(`Success, id: ${data.id} was posted at ${data.url}`);
-            console.log( `${plaqueDescriptionEnglish || ''}` );
             console.log('Amsterart Bot is shutting down')
             console.log('---- ^-^ ----')
         }
     })
   });
-
 
 //   Some helper functions
 
